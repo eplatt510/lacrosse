2ba2d0ce91a29fd6ddbe4b9635607d6fbcd95f3e
eplatt510 <eplatt510@gmail.com>
2017-03-13 10:50:41 -0700

    initial commit

---
+++ js/index.js
@@ -0,0 +1,143 @@
+//Faceoff
+var player1 = 0;
+var player2 = 0;
+var faceoffWinner = 0;
+var faceoffs;
+var faceoff = document.querySelector("#faceoff");
+var faceoffModal = document.querySelector("#faceoffModal")
+//var span = document.querySelector(".close");
+var faceoffPlayers = document.querySelector("#faceoffPlayers");
+var faceoffWinnerModal = document.querySelector("#faceoffWinnerModal");
+var faceOffForm = document.querySelector("#faceOffForm");
+var player1Display = document.querySelector("#player1Wins");
+var player2Display = document.querySelector("#player2Wins");
+var close = document.getElementsByClassName("close");
+
+if(!localStorage.getItem(faceoffs)){
+  faceoffs = 0;
+}
+faceoff.addEventListener("click", function(){
+  faceoffModal.style.display = "block";
+})
+
+// When the user clicks on close (x), close the modal
+for (var i = 0; i < close.length; i++){
+  close[i].addEventListener("click", function(){
+    faceoffModal.style.display = "none";
+    faceoffWinnerModal.style.display = "none";
+    groundBallModal.style.display = "none";
+    assistModal.style.display = "none";
+    shotModal.style.display = "none";
+    penaltyModal.style.display = "none";
+  })
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === faceoffModal) {
+        faceoffModal.style.display = "none";
+    }
+}
+
+faceoffPlayers.addEventListener("click", function(){
+  faceoffModal.style.display = "none";
+  player1 = document.querySelector("#player1").value;
+  player2 = document.querySelector("#player2").value;
+
+  faceoffWinnerModal.style.display = "block";
+  player1Display.textContent = player1;
+  player2Display.textContent = player2;
+  document.querySelector("#faceOffForm").reset();
+})
+
+player1Display.addEventListener("click", function(){
+  faceoffWinner = player1;
+  localStorage.setItem(player1, faceoffs++);
+  faceoffWinnerModal.style.display = "none";
+  alert(player1 + " has won " + faceoffs + " faceoffs.");
+
+})
+
+player2Display.addEventListener("click", function(){
+  faceoffWinner = player1;
+  localStorage.setItem(player1, faceoffs++);
+  faceoffWinnerModal.style.display = "none";
+  alert(player1 + " has won " + faceoffs + " faceoffs.");
+
+})
+
+//Ground Ball
+var groundBall = document.querySelector("#groundBall");
+var groundBallModal = document.querySelector("#groundBallModal");
+var groundBallWins = document.querySelector("#groundBallWins");
+var groundBallWinner = 0;
+
+groundBall.addEventListener("click", function(){
+  groundBallModal.style.display = "block";
+})
+
+groundBallWins.addEventListener("click", function(){
+  groundBallWinner = document.querySelector("#groundBallWinner").value;
+  groundBallModal.style.display = "none";
+  alert(groundBallWinner + " got the Ground Ball");
+})
+
+//Assist
+var assist = document.querySelector("#assist");
+var assistModal = document.querySelector("#assistModal");
+var assistSubmit = document.querySelector("#assistSubmit");
+var scorePlayer = 0;
+var assistPlayer = 0;
+
+
+assist.addEventListener("click", function(){
+  assistModal.style.display = "block";
+})
+
+assistSubmit.addEventListener("click", function(){
+  scorePlayer = document.querySelector("#scorePlayer").value;
+  assistPlayer = document.querySelector("#assistPlayer").value;
+  assistModal.style.display = "none";
+  alert(scorePlayer + " scored and " + assistPlayer + " assisted!");
+})
+
+//Shot
+var shot = document.querySelector("#shot");
+var shotModal = document.querySelector("#shotModal");
+var shotPlayer = document.querySelector("#shotPlayer");
+var shotTaker = document.querySelector("#shotTaker");
+
+shot.addEventListener("click", function(){
+  shotModal.style.display = "block";
+})
+
+shotPlayer.addEventListener("click", function(){
+  shotTaker = document.querySelector("#shotTaker").value;
+  shotModal.style.display = "none";
+  alert(shotTaker + " took a shot");
+
+})
+
+//Penalty
+var penalty = document.querySelector("#penalty");
+var penaltyAgainst = document.querySelector("#penaltyAgainst");
+var penaltyList = document.querySelector("#penaltyList");
+var penaltyTime = document.querySelector("#penaltyTime");
+var penaltySubmit = document.querySelector("#penaltySubmit");
+var penaltyForm = document.querySelector("#penaltyForm");
+
+
+penalty.addEventListener("click", function(){
+  penaltyModal.style.display = "block";
+})
+
+penaltySubmit.addEventListener("click", function(){
+  penaltyAgainst = penaltyAgainst.value;
+  penaltyList = penaltyList.value;
+  penaltyTime = penaltyTime.value;
+  penaltyModal.style.display = "none";
+  alert(penaltyAgainst + " committed a " + penaltyTime + " penalty for " + penaltyList);
+  penaltyForm.reset();
+})
+
+//Golie Save
+var goalieSave = document.querySelector("#goalieSave");
